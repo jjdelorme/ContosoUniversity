a1b15ec8c3bab9faf2887ee05ceae002de01f74f
jasondel <jasondel@WINDOWS2016-SAM>
2021-06-03 21:06:45 +0000

    Removing Google Diagnostics for demo initial state.

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Google.Cloud.Diagnostics.AspNetCore;
 
 namespace ContosoUniversity
 {

011db71cf54a345ec5be83458d1bd4ea3492e91c
Injae Kwak <ikwak@google.com>
2021-10-09 08:04:07 +1100

    TF instruction update for cloud shell

--- terraform-sandbox-setup/versions.tf
+++ terraform-sandbox-setup/versions.tf
@@ -19,10 +19,6 @@ terraform {
   required_providers {
     google = ">= 3.43, <4.0"
   }
-  backend "gcs" {
-  bucket = "contosouniversity-tf"
-  prefix = "terraform/state"
-}
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v7.1.0"
   }
